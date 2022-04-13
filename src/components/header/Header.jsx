fb657e28c88e122a4cf311176198d271a678e480
kingorianderson <kingorianderson@gmail.com>
2022-04-13 20:27:34 +0300

    shrinkHeader

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -24,6 +24,23 @@ const Header = () => {
 
   const active = headerNav.findIndex((e) => e.path === pathname);
 
+  useEffect(() => {
+    const shrinkHeader = () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
+        headerRef.current.classList.add("shrink");
+      } else {
+        headerRef.current.classList.remove("shrink");
+      }
+    };
+    window.addEventListener("scroll", shrinkHeader);
+    return () => {
+      window.removeEventListener("scroll", shrinkHeader);
+    };
+  }, []);
+
   return (
     <div ref={headerRef} className="header">
       <div className="header__wrap container">
