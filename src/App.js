4ec59a885511811d2d0b96c9406b5d17a676173c
kingorianderson <kingorianderson@gmail.com>
2022-04-13 20:18:33 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Catalog from "./pages/Catalog";
+import Detail from "./pages/detail/Detail";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/:category/search/:keyword" element={<Catalog />} />
+        <Route path="/:category/:id" element={<Detail />} />
+        <Route path="/:category" element={<Catalog />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
