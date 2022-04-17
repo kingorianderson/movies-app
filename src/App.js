cc0066022201208d57448596e48f976388ff6e24
kingorianderson <kingorianderson@gmail.com>
2022-04-17 15:49:55 +0300

    App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.scss";
+import "swiper/swiper.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
