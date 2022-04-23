ffa51e5b11cb8007785595baced7ca9f9d57bc89
kingorianderson <kingorianderson@gmail.com>
2022-04-23 19:49:35 +0300

    added backgroundImage

--- src/components/pageHeader/PageHeader.jsx
+++ src/components/pageHeader/PageHeader.jsx
@@ -1,8 +1,9 @@
 import "./pageHeader.scss";
+import Bg from "../../assets/footer-bg.jpg";
 
 const PageHeader = (props) => {
   return (
-    <div className="page-header">
+    <div className="page-header" style={{ backgroundImage: `url(${Bg})` }}>
       <h2>{props.children}</h2>
     </div>
   );
