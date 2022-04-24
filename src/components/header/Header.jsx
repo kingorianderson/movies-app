4bc909b915a71663acc8b4601c801c533cf1c071
kingorianderson <kingorianderson@gmail.com>
2022-04-24 13:30:20 +0300

    changed the name to 'uhdMovies'

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
       <div className="header__wrap container">
         <div className="logo">
           <img src={Logo} alt="" />
-          <Link to="/">netMovies</Link>
+          <Link to="/">uhdMovies</Link>
         </div>
         <ul className="header__nav">
           {headerNav.map((e, i) => (
