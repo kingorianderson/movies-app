ec4ec6800ea9b82716979930e6bbb9620ffbea9d
kingorianderson <kingorianderson@gmail.com>
2022-04-24 13:29:47 +0300

    changed the name to 'uhdMovies'

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className="footer__content__logo">
           <div className="logo">
             <img src={Logo} alt="" />
-            <Link to="/">netMovies</Link>
+            <Link to="/">uhdMovies</Link>
           </div>
         </div>
         <div className="footer__content__menus">
